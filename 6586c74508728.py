0e8c3433269087632ba76ce0fafafaa469e49951
danilpustomelnik <144527154+danilpustomelnik@users.noreply.github.com>
2024-01-03 15:38:51 +0200

    Add files via upload

---
+++ 6586c74508728.py
@@ -0,0 +1,52 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import*
+
+app = QApplication([])
+
+'''Інтерфейс програми'''
+# параметри вікна програми
+window = QWidget()
+window.setWindowTitle('Цитатник')
+window.resize(900, 600)
+
+label1= QLabel('''Один в поле не воробей, туда сюда и сто рублей.
+        Напиши свои цитаты.''')
+
+text1=QTextEdit()
+
+text2=QLineEdit('')
+
+label2= QLabel('Подпиши свои великолепные цитаты (Имя Фамилия)')
+
+b1=QPushButton("Ok")
+
+v=QVBoxLayout()
+
+v.addWidget(label1,alignment=Qt.AlignCenter)
+v.addWidget(text1)
+v.addWidget(text2)
+v.addWidget(label2,alignment=Qt.AlignCenter)
+
+v.addWidget(b1)
+
+
+window.setLayout(v)
+
+index=0
+def babam():
+    global index
+    info_win=QMessageBox()
+    info_win.setWindowTitle('Загрузка цитат')
+    info_win.setText('Цитаты загружены, спасибо \n'+text2.text())
+    info_win.exec_()
+    with open("1.txt", "a",encoding="utf-8") as file:
+        file.write("Имя: "+text2.text()+'\n')
+        file.write("Цитаты: "+text1.toPlainText()+'\n')
+
+
+
+
+# запуск програми
+b1.clicked.connect(babam)
+window.show()
+app.exec_()
